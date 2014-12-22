8e48ae9db7875bb453beb9d48f9ff8d75ef5fbc5
Aaron Cruz <aaron@aaroncruz.com>
2014-12-22 11:47:28 +0100

    Search and latest endpoints working

--- commands/server.go
+++ commands/server.go
@@ -1,21 +1,29 @@
 package commands
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"bitbucket.org/pferdefleisch/dbpm/data"
+	"bitbucket.org/pferdefleisch/dbpm/models"
+
+	"github.com/jmoiron/sqlx"
 	"github.com/julienschmidt/httprouter"
 )
 
 const _port = "4567"
 
+var db *sqlx.DB
+
 // Server runs the web server that runs thepickmachine
 func Server() {
+	db = data.DBInstance()
 	router := httprouter.New()
 	router.GET("/", index)
-	router.GET("/hello/:name", hello)
+	router.GET("/search", search)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -27,9 +35,43 @@ func Server() {
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+	pickModel := &models.Pick{}
+	picks, err := pickModel.Latest(db)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	jsn, err := json.Marshal(picks)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsn)
 }
 
-func hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+func search(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	term := r.FormValue("q")
+	showSlug := r.FormValue("show")
+
+	var err error
+	picks := []models.FullPick{}
+	pickModel := &models.Pick{}
+	if showSlug == "" {
+		picks, err = pickModel.AllSearch(db, term)
+	} else {
+		picks, err = pickModel.ShowSearch(db, term, showSlug)
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	jsn, err := json.Marshal(picks)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsn)
 }
--- models/pick.go
+++ models/pick.go
@@ -1,19 +1,36 @@
 package models
 
 import (
+	"time"
+
 	"bitbucket.org/pferdefleisch/dbpm/clients"
 	"github.com/jmoiron/sqlx"
 )
 
 // Pick model that is a db and json model
 type Pick struct {
-	ID          int
-	EpisodeID   int `db:"episode_id"`
-	Host        string
-	Name        string
-	Link        string
-	Description string
-	Content     string
+	ID          int    `db:"id"`
+	EpisodeID   int    `db:"episode_id"`
+	Host        string `db:"host"`
+	Name        string `db:"name"`
+	Link        string `db:"link"`
+	Description string `db:"description"`
+	Content     string `db:"content"`
+}
+
+// FullPick represents a pick with episode and show data
+type FullPick struct {
+	Host               string    `json:"host" db:"host"`
+	Name               string    `json:"name" db:"name"`
+	Link               string    `json:"link" db:"link"`
+	Description        string    `json:"description" db:"description"`
+	Number             string    `json:"number" db:"number"`
+	EpisodeTitle       string    `json:"episode_title" db:"episode_title"`
+	EpisodeDescription string    `json:"episode_description" db:"episode_description"`
+	EpisodeSlug        string    `json:"episode_slug" db:"episode_slug"`
+	ShowName           string    `json:"show_name" db:"show_name"`
+	ShowSlug           string    `json:"show_slug" db:"show_slug"`
+	PublishedAt        time.Time `json:"published_at" db:"published_at"`
 }
 
 // Save saves pick to database
@@ -39,6 +56,82 @@ func (p *Pick) Save(db *sqlx.DB) error {
 		p.ID = id
 	}
 	return nil
+}
+
+// Latest gets a list of latest picks
+func (p *Pick) Latest(db *sqlx.DB) ([]FullPick, error) {
+	query := `
+		SELECT
+			p.host, p.name, p.link, p.description,
+			e.number, e.published_at, e.title episode_title,
+			e.slug episode_slug, e.description episode_description,
+			s.name show_name, s.slug show_slug
+		FROM picks AS p
+	  LEFT JOIN episodes AS e
+			ON p.episode_id = e.id
+		LEFT JOIN shows AS s
+			ON e.show_id = s.id
+		ORDER BY e.published_at DESC
+		LIMIT 30
+	`
+	picks := []FullPick{}
+	err := db.Select(&picks, query)
+	if err != nil {
+		return nil, err
+	}
+	return picks, nil
+}
+
+// ShowSearch searches a show for picks
+func (p *Pick) ShowSearch(db *sqlx.DB, term, showSlug string) ([]FullPick, error) {
+	query := `
+    SELECT
+			p.host, p.name, p.link, p.description,
+			e.number, e.published_at, e.title episode_title,
+			e.slug episode_slug, e.description episode_description,
+			s.name show_name, s.slug show_slug
+		FROM picks AS p
+	  LEFT JOIN episodes AS e
+			ON p.episode_id = e.id
+		LEFT JOIN shows AS s
+			ON e.show_id = s.id
+    WHERE
+      to_tsvector('english', p.name || ' ' || p.description || ' ' || p.content) @@
+      to_tsquery('english', $1)
+		AND
+		   s.slug = $2
+		`
+	picks := []FullPick{}
+	err := db.Select(&picks, query, term, showSlug)
+	if err != nil {
+		return nil, err
+	}
+	return picks, nil
+}
+
+// AllSearch searches all shows for picks
+func (p *Pick) AllSearch(db *sqlx.DB, term string) ([]FullPick, error) {
+	query := `
+    SELECT
+			p.host, p.name, p.link, p.description,
+			e.number, e.published_at, e.title episode_title,
+			e.slug episode_slug, e.description episode_description,
+			s.name show_name, s.slug show_slug
+		FROM picks AS p
+	  LEFT JOIN episodes AS e
+			ON p.episode_id = e.id
+		LEFT JOIN shows AS s
+			ON e.show_id = s.id
+    WHERE
+      to_tsvector('english', p.name || ' ' || p.description || ' ' || p.content) @@
+      to_tsquery('english', $1)
+		`
+	picks := []FullPick{}
+	err := db.Select(&picks, query, term)
+	if err != nil {
+		return nil, err
+	}
+	return picks, nil
 }
 
 // UpdateContent just updates the content field of pick
