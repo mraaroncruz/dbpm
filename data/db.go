196b4fb34df453acaf37cc2821f8496ecc2030e5
Aaron Cruz <aaron@aaroncruz.com>
2014-12-23 13:35:30 +0100

    database now takes a config

--- data/db.go
+++ data/db.go
@@ -2,12 +2,44 @@ package data
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/jmoiron/sqlx"
+	yaml "gopkg.in/yaml.v2"
 )
 
+type dbConfig struct {
+	Database dbase
+}
+
+type dbase struct {
+	User     string
+	Password string
+	Name     string
+}
+
+// DBInstance returns an instance of the db to use in the application
 func DBInstance() *sqlx.DB {
-	db, err := sqlx.Connect("postgres", "user=al dbname=dbpm_development port=5432 sslmode=disable")
+	filePath := os.Getenv("CONFIG")
+	if filePath == "" {
+		filePath = "./config.yml"
+	}
+	configBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatalf("Fatal: %s\n", err)
+	}
+
+	config := dbConfig{}
+	err = yaml.Unmarshal(configBytes, &config)
+	if err != nil {
+		log.Fatalf("Fatal: %s\n", err)
+	}
+
+	cdb := config.Database
+	configString := fmt.Sprintf("user=%s password=%s dbname=%s port=5432 sslmode=disable", cdb.User, cdb.Password, cdb.Name)
+	db, err := sqlx.Connect("postgres", configString)
 	if err != nil {
 		fmt.Printf("DB connection error: %s\n\n", err)
 	}
